7530169625d3b59b7fd58c6754df0f8ad239a61d
mramshaw1 <m.ramshaw@hotmail.co.uk>
2023-04-04 17:19:06 +0100

    A completed console log game of rock papaper scissors, still work to do on the UI side

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const round = playRound()
-const computerSelection = getComputerChoice();
-const playerSelection = "scissors";
+let playerScore = 0
+let computerScore = 0
+
+
+
+
 
 function getComputerChoice(){
     const choicesArr = ["rock", "paper", "scissors"];
@@ -14,23 +17,47 @@ function playRound(playerSelection, computerSelection){
     if (playerSelection == computerSelection){
         return ("Round is a TIE");
     } else if (playerSelection == "rock" && computerSelection == "paper"){
+        computerScore++
         return ("You lose! Paper beats Rock");
     } else if (playerSelection == "rock" && computerSelection == "scissors"){
+        playerScore++
         return ("You win! Rock beats Paper");
     }else if (playerSelection == "scissors" && computerSelection == "paper"){
+        playerScore++
         return ("You win! Scissors beats Paper");
     }else if (playerSelection == "scissors" && computerSelection == "rock"){
-            return ("You lose! Rock beats Scissors");
+        computerScore++    
+        return ("You lose! Rock beats Scissors");
     }else if (playerSelection == "paper" && computerSelection == "rock"){
-                return ("You win! Paper beats Rock");
+        playerScore++        
+        return ("You win! Paper beats Rock");
     }else if (playerSelection == "paper" && computerSelection == "scissors"){
-                    return ("You lose! Scissors beats Paper");
+        computerScore++
+        return ("You lose! Scissors beats Paper");
     }}
 
 
 
-
 function game(){
-    for (i = 1; i<=5; i++)
+    for (let i = 1; i<=5; i++){
+            const computerSelection = getComputerChoice()
+            const playerSelection = prompt("Pick from Rock, Paper or Scissors")
 
+        console.log(playRound(playerSelection, computerSelection))
+    
+        console.log(playerScore, computerScore)
 }
+if (playerScore>computerScore){
+    return "You beat the comp"
+} else if (playerScore< computerScore){
+    return "Comp beat you"
+} else {
+return "Draw"
+}
+}
+
+
+
+
+
+console.log(game());
